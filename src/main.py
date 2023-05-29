f719f8dbe301e4565a10c7925192b95c0884d4f9
Darley Barreto <darleybarreto@gmail.com>
2023-05-29 09:44:31 -0300

    Fixing game selection

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import time
 import argparse
+from sys import exit
+from datetime import datetime
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -22,40 +24,55 @@ def set_chrome_options() -> Options:
 
 def main():
     parser = argparse.ArgumentParser(
-                    prog='Estrela Scraper',
-                    description="Scrape Estrela Bet's website")
+                    prog='Star Bot',
+                    description="Scrape data from Estrela Bet's website")
 
     parser.add_argument('team')
+    parser.add_argument("-t", "--timeout",
+                        default=5,
+                        type=int,
+                        help="Timeout to wait for Chromium webengine load website.")
     args = parser.parse_args()
     team = args.team.lower()
 
     driver = webdriver.Chrome(options=set_chrome_options())
     driver.get(URL.format(team))
 
-    css_selector = "#container-main > app-fixture-search > div.srch.page > div.modul-accordion.sportType"
-    
+    css_selector = "#container-main > app-fixture-search > div.srch.page > div.modul-accordion.sportType > div.modul-content"
+    time.sleep(10)
+
     try:
-        time.sleep(5)
         el = driver.find_element(By.CSS_SELECTOR, css_selector)
     except:
-        print("No game found")
+        print("No games found or timeout needs to be increased")
+        exit(1)
 
-    children = el.find_elements(By.XPATH,"*")
-    games = children[-1].find_elements(By.XPATH,"*")
+    leagues = el.find_elements(By.XPATH,"*")
     
-    if len(games) > 0:
-        first_game_table = games[0].find_elements(By.XPATH,"*")
-        table = first_game_table[-1]\
-            .find_element(By.CSS_SELECTOR, "div.fixture-container")\
-            .find_elements(By.XPATH, "*")[2]
-        columns = table.text.split("\n")
+    odds_dict = {}
+
+    for i,l in enumerate(leagues, start=1):
+        inner_div_content = l.find_element(By.CSS_SELECTOR,f"div:nth-child({i}) > div.modul-content > div")
+        inner_elements = inner_div_content.find_elements(By.XPATH,"*")
+
+        if len(inner_elements) == 0:
+            continue
+
+        date_str = inner_elements[1].text.rstrip().strip().split()[-1]
+        date = datetime.strptime(date_str, '%d/%m/%Y')
+        
+        columns = inner_elements[2].text.split("\n")
         index = [i for i, e in enumerate(columns) if e == args.team][-1]
-        odds = float(columns[index - 1])
-        print(f"The odds of {args.team} winning the next game are {odds}")
+        odds_dict[date] = float(columns[index - 1])
 
+    if len(odds_dict) == 0:
+        print("No games found")
     else:
-        print("No game found")
-    
+        sorted_dates = sorted(odds_dict)
+        next_date = sorted_dates[0]
+        odds = odds_dict[next_date]
+        pprint_date = next_date.strftime("%d/%m/%Y")
+        print(f"The odds of {args.team} winning the next game on {pprint_date} are {odds}")
     
     driver.close()
 
